8c9e62e7ceecc3024286c72711d2783f7d1de01c
Rosana <rosana.patino@es.schunk.com>
2024-12-26 18:09:37 +0100

    🧑‍💻 theme and type

--- lib/auth/screens/login_screen.dart
+++ lib/auth/screens/login_screen.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  LoginScreenState createState() => LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController pwdController = TextEditingController();
+
     return Scaffold(
         appBar: AppBar(
-      title: const Text('login screen'),
-    ));
+          title: const Text('login screen'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              children: [
+                TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                    hintText: 'Enter your email',
+                    labelText: 'Email',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: pwdController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                    hintText: 'Enter your password',
+                    labelText: 'Password',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: (() {}),
+                  child: const Text('Login'),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
